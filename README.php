4cdeb34caf1998e1b4ebb730beed1cb2df58cac6
Walter Dal Mut <walter.dalmut@gmail.com>
2013-01-13 16:26:15 +0000

    examples

--- README.php
+++ README.php
@@ -4,3 +4,11 @@ Sentiment analysis with PHP
 
 Based on a great article http://www.phpir.com/bayesian-opinion-mining
 
+## Example
+
+```
+Classifying 'Avatar had a surprisingly decent plot, and genuinely incredible special effects' - pos
+Classifying 'Twilight was an atrocious movie, filled with stumbling, awful dialogue, and ridiculous story telling.' - neg
+Classifying 'Loving this wheater' - pos
+```
+
